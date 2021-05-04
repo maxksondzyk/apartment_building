0acf2020d2e691b32d072d41be95049da931d8ab
Max Ksondzyk <maxksiondzyk@gmail.com>
2021-05-04 16:58:12 +0300

    Some interface improvements + apartments info

--- src/components/apartment.component.js
+++ src/components/apartment.component.js
@@ -1,28 +1,20 @@
 import React, { Component } from 'react';
-import axios from 'axios';
 
 import "../styles/apartment.styles.css"
 
-
-
-let typeApt = "";
-let floor = ""
-
-// const AnApartment = props => (
-//     <tr>
-//         <td>{props.apartment.number}</td>
-//         <td>{props.apartment.floor}</td>
-//         <td>{props.apartment.rooms}</td>
-//         <td>{props.apartment.total_area}</td>
-//         <td>{props.apartment.living_area}</td>
-//         <td>{props.apartment.availability}</td>
-//         <td>{props.apartment.price}</td>
-//     </tr>
-// );
-
-const ApartmentFloor = props => (
-    <option>{props.apartment.floor}</option>
+const AnApartment = props => (
+    <div className={"aptInfo"}>
+        <h3>Number: {props.apartment.number}</h3>
+        <h3>Floor: {props.apartment.floor}</h3>
+        <h3>Rooms: {props.apartment.rooms}</h3>
+        <h3>Area: {props.apartment.total_area}</h3>
+        <h3>Price: {props.apartment.price}</h3>
+    </div>
 );
+
+// const ApartmentFloor = props => (
+//     <option>{props.apartment.floor}</option>
+// );
 
 
 export default class Apartment extends Component {
@@ -31,52 +23,35 @@ export default class Apartment extends Component {
 
 
         if(props.location.state){
-            typeApt = props.location.state;
-            floor = props.location.floor;
+            this.state = { apartment: props.location.state};
         }
-        alert(floor)
-
 
         console.log(props.location.state);
-        this.state = { apartments: []};
-    }
 
-    componentDidMount() {
-        axios.get('http://localhost:5000/apartments/')
-            .then(response => {
-                this.setState({ apartments: response.data })
-
-            })
-            .catch((error) => {
-                console.log(error);
-            })
     }
 
     apartmentsList() {
-        return this.state.apartments.map(currentapartment => {
-            return currentapartment.availability === true && currentapartment.planning === typeApt ? <ApartmentFloor apartment={currentapartment}  key={currentapartment._id}/> : null;
-        })
+        if(this.state) {
+            return <AnApartment apartment={this.state.apartment} key={this.state.apartment._id}/>;
+        }
     }
 
    render() {
-        let image;
-        if(typeApt==="apt21") {
-            image = "https://i.ibb.co/ZxP9DcS/apt21.png";
-        }
-        else if(typeApt==="apt22"){
-            image = "https://i.ibb.co/n60rPFs/apt22.png";
-        }
-        else if(typeApt==="apt23"){
-            image = "https://i.ibb.co/xzLzCqK/apt23.png";
-        }
-        else if(typeApt==="apt24"){
-            image = "https://i.ibb.co/9nQ7080/apt24.png";
-        }
-        else if(typeApt==="apt25"){
-            image = "https://i.ibb.co/bXWrByx/apt25.png";
-        }
-        else {
-            image = "https://i.ibb.co/XbNXjsg/apt26.png";
+       let image;
+        if(this.state) {
+            if (this.state.apartment.planning === "apt21") {
+                image = "https://i.ibb.co/ZxP9DcS/apt21.png";
+            } else if (this.state.apartment.planning === "apt22") {
+                image = "https://i.ibb.co/n60rPFs/apt22.png";
+            } else if (this.state.apartment.planning === "apt23") {
+                image = "https://i.ibb.co/xzLzCqK/apt23.png";
+            } else if (this.state.apartment.planning === "apt24") {
+                image = "https://i.ibb.co/9nQ7080/apt24.png";
+            } else if (this.state.apartment.planning === "apt25") {
+                image = "https://i.ibb.co/bXWrByx/apt25.png";
+            } else {
+                image = "https://i.ibb.co/XbNXjsg/apt26.png";
+            }
         }
        return (
            <div>
@@ -102,13 +77,13 @@ export default class Apartment extends Component {
                    <div className="col-md-6 plan-div">
                        <img src={image} width="100%" alt="Apartment plan"/>
                    </div>
-                   <div className="col-sm-1">
-                       <label htmlFor="exampleFormControlSelect1">Floor</label>
-                       <select className="form-control" id="exampleFormControlSelect1">
+                   <div className="col-sm-5">
+                       {/*<label htmlFor="exampleFormControlSelect1">Floor</label>*/}
+                       {/*<select className="form-control" id="exampleFormControlSelect1">*/}
                            {
                                this.apartmentsList()
                            }
-                       </select>
+                       {/*</select>*/}
                    </div>
                </div>
            </div>
--- src/components/plan.component.js
+++ src/components/plan.component.js
@@ -2,101 +2,105 @@ import React from 'react';
 
 import "../styles/plan.styles.css"
 import { Link } from 'react-router-dom';
-import planImg from '../images/main_plan.png'
 
-let floor = ""
 class Plan extends React.Component {
     constructor(props) {
         super(props);
-
-
         if(props.location.state){
-            floor = props.location.state;
+            this.state = { apartments: props.location.state};
         }
 
-
-        console.log(props.location.state);
-        this.state = { apartments: []};
+    }
+    getApartment(planning){
+        let apt;
+        this.state.apartments.forEach(apartment => {
+            if(apartment.planning === planning) {
+                apt = apartment
+                // return apartment;
+            }
+        })
+        return apt;
+        // return (this.state.apartments.filter(currentApartment =>
+        //     currentApartment.planning === planning
+        // ))
     }
     render() {
 
         return (
-            <div className="cont">
-                <img className="planImg" src={"https://i.ibb.co/pw17bbw/Main-plan.png"} alt={"Apartments plan"}/>
-                <svg viewBox="0 50 900 400">
-                    <text x="120" y="125" fill="white">APT21</text>
-                    <g transform="scale(0.16)">
-                        <Link to={
-                        {
-                            pathname: '/apartment',
-                            state:"apt22",
-                            floor:floor
-                        }
-                        }>
-                        <path d="m587.75 2380.85 -0.65 -873.78 997.33 -0.85v0.00 445.88l163.65 0.62 0.31 427.47z"
-                              data-code="apt22"
-                              className="jvectormap-region jvectormap-element apt"/>
-                        </Link>
-                        <Link to={
-                            {
-                                pathname: '/apartment',
-                                state:"apt23",
-                                floor:floor
-                            }
-                        }>
-                        <path d="m1816.74 607.25 1321.52 0.20 0.20 1066.01 -924.48 0.19 -0.08 -908.79 -396.95 -0.15z"
-                              data-code="apt23"
-                              className="jvectormap-region jvectormap-element apt"/>
-                        </Link>
-                        <Link to={
-                            {
-                                pathname: '/apartment',
-                                state:"apt24",
-                                floor:floor
-                            }
-                        }>
-                        <path d="m3164.73 1673.24 0.43 -1065.17 868.49 -0.31 -0.31 1066.35z"
-                              data-code="apt24"
-                              className="jvectormap-region jvectormap-element apt"/>
-                        </Link>
-                        <Link to={
-                            {
-                                pathname: '/apartment',
-                                state:"apt21",
-                                floor:floor
-                            }
-                        }>
-                        <path d="m587.75 1480.81 -0.22 -873.23 1203.17 0.44v0.00 913.55l-179.79 -0.44 -0.22 -41.41z"
-                              data-code="apt21"
-                              className="jvectormap-region jvectormap-element apt"/>
-                        </Link>
-                        <Link to={
-                            {
-                                pathname: '/apartment',
-                                state:"apt26",
-                                floor:floor
-                            }
-                        }>
-                        <path
-                            d="m2187.57 2380.96 -0.02 -428.55 2349.08 0.01 0.77 456.44h-478.63 0.00l-0.31 -34.21 -16.95 0.31 -0.15 5.70z"
-                            data-code="apt26"
-                            className="jvectormap-region jvectormap-element apt"/>
-                        </Link>
-                        <Link to={
-                            {
-                                pathname: '/apartment',
-                                state:"apt25",
-                                floor:floor
-                            }
-                        }>
-                        <path
-                            d="m4059.32 1673.46 -0.22 -1119.71 1111.21 0.65 0.87 400.55 -380.06 0.87v0.00 400.77l-228.14 1.16 -0.03 315.71z"
-                            data-code="apt25"
-                            className="jvectormap-region jvectormap-element apt"/>
-                        </Link>
-                    </g>
-                </svg>
+            <div>
+                <div className={"floorNum"}>
+                    <h1>Floor {this.state.apartments[0].floor}</h1>
+                </div>
+                <div className="cont">
+                    <img className="planImg" src={"https://i.ibb.co/pw17bbw/Main-plan.png"} alt={"Apartments plan"}/>
+                    <svg viewBox="0 50 900 400">
 
+                        <g transform="scale(0.16)">
+                            <Link to={
+                            {
+                                pathname: '/apartment',
+                                state: this.getApartment("apt22"),
+                            }
+                            }>
+                            <path d="m587.75 2380.85 -0.65 -873.78 997.33 -0.85v0.00 445.88l163.65 0.62 0.31 427.47z"
+                                  data-code="apt22"
+                                  className="jvectormap-region jvectormap-element apt"/>
+                            </Link>
+                            <Link to={
+                                {
+                                    pathname: '/apartment',
+                                    state: this.getApartment("apt23"),
+                                }
+                            }>
+                            <path d="m1816.74 607.25 1321.52 0.20 0.20 1066.01 -924.48 0.19 -0.08 -908.79 -396.95 -0.15z"
+                                  data-code="apt23"
+                                  className="jvectormap-region jvectormap-element apt"/>
+                            </Link>
+                            <Link to={
+                                {
+                                    pathname: '/apartment',
+                                    state: this.getApartment("apt24"),
+                                }
+                            }>
+                            <path d="m3164.73 1673.24 0.43 -1065.17 868.49 -0.31 -0.31 1066.35z"
+                                  data-code="apt24"
+                                  className="jvectormap-region jvectormap-element apt"/>
+                            </Link>
+                            <Link to={
+                                {
+                                    pathname: '/apartment',
+                                    state: this.getApartment("apt21"),
+                                }
+                            }>
+                            <path d="m587.75 1480.81 -0.22 -873.23 1203.17 0.44v0.00 913.55l-179.79 -0.44 -0.22 -41.41z"
+                                  data-code="apt21"
+                                  className="jvectormap-region jvectormap-element apt"/>
+                            </Link>
+                            <Link to={
+                                {
+                                    pathname: '/apartment',
+                                    state: this.getApartment("apt26"),
+                                }
+                            }>
+                            <path
+                                d="m2187.57 2380.96 -0.02 -428.55 2349.08 0.01 0.77 456.44h-478.63 0.00l-0.31 -34.21 -16.95 0.31 -0.15 5.70z"
+                                data-code="apt26"
+                                className="jvectormap-region jvectormap-element apt"/>
+                            </Link>
+                            <Link to={
+                                {
+                                    pathname: '/apartment',
+                                    state: this.getApartment("apt25"),
+                                }
+                            }>
+                            <path
+                                d="m4059.32 1673.46 -0.22 -1119.71 1111.21 0.65 0.87 400.55 -380.06 0.87v0.00 400.77l-228.14 1.16 -0.03 315.71z"
+                                data-code="apt25"
+                                className="jvectormap-region jvectormap-element apt"/>
+                            </Link>
+                        </g>
+                    </svg>
+                </div>
             </div>
         );
     }
